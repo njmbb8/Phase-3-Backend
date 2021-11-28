b6d4dc38666b338b5338693b96ae313e89c3afb0
njmbb8 <nmcgr001@gmail.com>
2021-11-27 19:53:07 -0500

    Adding new items works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/items" do
+    Item.all.to_json
   end
 
+  post '/items/' do
+    item = Item.create(
+      name: params[:name],
+      image: params[:image],
+      price: params[:price]
+    )
+
+    item.to_json
+  end
 end
